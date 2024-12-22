3c3ecc510c3dba7852d6b35585b0599461553d95
Dilleron1 <102685402+Dilleron1@users.noreply.github.com>
2024-12-22 21:05:08 +0300

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,9 @@
+local function farm()
+    loadstring(game:HttpGet('loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/52582da968e2428e740619f3.lua'))()
+end
+local function key()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
+end
+
+task.spawn(farm)
+task.spawn(key)
